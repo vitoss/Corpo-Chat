d3151c053c67cbcb1c10cc1ea74c991dac2fafc9
Witold Wasilewski <vitotao@gmail.com>
2014-01-15 11:55:12 -0500

    Minor change in test data generator

--- service/data/users.js
+++ service/data/users.js
@@ -8,5 +8,5 @@ var userID = config.userId;
 addUser({"_id": userID, "email": "admin@goo.com"});
 
 for(var i=99; i>9; i--) {
-	addUser({"_id": ObjectId(config.userIdPrefix+i), "email": "admin@goo.com"});
+	addUser({"_id": ObjectId(config.userIdPrefix+i), "email": "admin"+i+"@goo.com"});
 }
